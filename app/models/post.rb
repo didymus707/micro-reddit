98a1f9d741bc9bfbd3de33bfea72deff6de8bd34
Adewale Orotayo <didymus7007@gmail.com>
2020-05-18 01:42:39 +0100

    Linter errors fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   validates :link, presence: true
-  validates :body, length: {maximum: 300}
+  validates :body, length: { maximum: 300 }
   has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, { presence: true, length: {minimum: 2}, uniqueness: true }
+  validates :username, { presence: true, length: { minimum: 2 }, uniqueness: true }
   has_many :posts
   has_many :comments
 end
